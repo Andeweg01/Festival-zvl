49ae793be27d1e8572b0866ef3e7dcb3b0937aa7
Sascha Andeweg <sascha@designed.irish>
2020-08-15 13:54:25 +0000

    small fixes in concerts.json and admin view customised

--- concerts/admin.py
+++ concerts/admin.py
@@ -1,9 +1,43 @@
 from django.contrib import admin
-from .models import Concert
-from .models import Edition
-from .models import Location
+from .models import Concert, Edition, Location
 
+class ConcertAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'concert_name',
+        'friendly_name',
+        'concert_date',
+        'edition',
+        'concert_subtitle',
+        'concert_theme',
+        'concert_conductor',
+        'concert_soloist',
+        'concert_program',
+        'concert_description',
+        'concert_time',
+        'concert_price',
+        'concert_availability',
+        'concert_image',
+        'location',
+    )
 
-admin.site.register(Concert)
-admin.site.register(Edition)
-admin.site.register(Location)
+    ordering = ('sku',)
+
+class EditionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'loc_name',
+        'friendly_name',
+        'loc_address',
+        'loc_pc',
+        'loc_place',
+    )
+
+admin.site.register(Concert, ConcertAdmin)
+admin.site.register(Edition, EditionAdmin)
+admin.site.register(Location, LocationAdmin)
--- concerts/models.py
+++ concerts/models.py
@@ -27,11 +27,13 @@ class Location(models.Model):
 
 
 class Concert(models.Model):
-    edition = models.ForeignKey('Edition', null=True, blank=True, on_delete=models.SET_NULL)
+    edition = models.ForeignKey(
+        'Edition', null=True, blank=True, on_delete=models.SET_NULL
+    )
     sku = models.CharField(max_length=254, null=True, blank=True)
     concert_name = models.CharField(max_length=254, null=True, blank=True)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    concert_date = models.CharField(max_length=254, null=True, blank=True)
+    concert_date = models.DateField(max_length=254, null=True, blank=True)
     concert_subtitle = models.CharField(max_length=254, null=True, blank=True)
     concert_theme = models.CharField(max_length=254, null=True, blank=True)
     concert_conductor = models.CharField(max_length=254, null=True, blank=True)
@@ -40,7 +42,11 @@ class Concert(models.Model):
     concert_description = models.TextField(null=True, blank=True)
     concert_time = models.TimeField(max_length=254, null=True, blank=True)
     concert_price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    concert_availability = models.DecimalField(max_digits=4, decimal_places=0, null=True, blank=True)
+    concert_availability = models.DecimalField(
+        max_digits=4, decimal_places=0, null=True, blank=True
+    )
     concert_url = models.URLField(max_length=1024, null=True, blank=True)
     concert_image = models.ImageField(max_length=254, null=True, blank=True)
-    location = models.ForeignKey('Location', null=True, blank=True, on_delete=models.SET_NULL)
+    location = models.ForeignKey(
+        'Location', null=True, blank=True, on_delete=models.SET_NULL
+    )
