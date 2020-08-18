53b85a93bc572037908a36194d8babe26344cc52
Sascha Andeweg <sascha@designed.irish>
2020-08-18 17:03:18 +0000

    added shopping cart template app urls

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_cart(request):
+    """ the view to return cart.html showing the contents of the chopping cart """
+    
+    return render(request, 'cart/cart.html')
