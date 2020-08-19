d795bc1d254741c266ed3ec34b891a9aa6330c72
Sascha Andeweg <sascha@designed.irish>
2020-08-19 20:41:32 +0000

    added add to cart functionality and smaller oister for small screens

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,17 @@ def view_cart(request):
     """ the view to return cart.html showing the contents of the chopping cart """
     
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
