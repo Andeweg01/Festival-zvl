928340bcbc0a37e5a4a07b0aa2cbab68a2d60f55
Sascha Andeweg <sascha@designed.irish>
2020-08-19 21:04:55 +0000

    fixed some typos in contexts.py

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from concerts.models import Concert
 
 
 def cart_contents(request):
@@ -7,6 +9,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     concert_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        concert = get_object_or_404(Concert, pk=item_id)
+        total += quantity * concert.concert_price
+        concert_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'concert': concert,
+        })
 
     grand_total = total
 
