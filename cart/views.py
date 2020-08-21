7df3bef5a63b5fc1a4caf813357306bb42acc631
Sascha Andeweg <sascha@designed.irish>
2020-08-21 16:54:14 +0000

    remove button is working

--- cart/views.py
+++ cart/views.py
@@ -21,29 +21,31 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     return redirect(redirect_url)
 
-# def adjust_cart(request, item_id):
-#     """Adjust the quantity of the specified product to the specified amount"""
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
 
-#     quantity = int(request.POST.get('quantity'))
-#     cart = request.session.get('cart', {})
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
 
-#     if quantity > 0:
-#         cart[item_id] = quantity
-#     else:
-#         cart.pop(item_id)
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
 
-#     request.session['cart'] = bag
-#     return redirect(reverse('view_cart'))
+    request.session['cart'] = cart
+    print('item_id')
+    return redirect(reverse('view_cart'))
 
 
-# def remove_from_cart(request, item_id):
-#     """Remove item from the cart"""
+def remove_from_cart(request, item_id):
+    """Remove item from the cart"""
 
-#     cart = request.session.get('cart', {})
-#     cart.pop(item_id)
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
 
-#     request.session['cart'] = cart
-#     return HttpResponse(status=200)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
 
-#     except Exception as e:
-#         return HttpResponse(status=500)
+    except Exception as e:
+        return HttpResponse(status=500)
