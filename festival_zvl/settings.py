165ca9f028972d7e935413e08a80b6a1a6f54160
Sascha Andeweg <sascha@designed.irish>
2020-08-22 18:54:47 +0000

    initial commit checkout form

--- festival_zvl/settings.py
+++ festival_zvl/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'concerts',
     'cart',
     'checkout',
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'festival_zvl.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
