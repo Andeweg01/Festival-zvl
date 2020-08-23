f4ee2bf4967578865e8e7e84760ec2dcdc1e027d
Sascha Andeweg <sascha@designed.irish>
2020-08-23 19:21:51 +0000

    finished the checkout process in views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
 from cart.contexts import cart_contents
 
 import stripe
@@ -12,21 +13,68 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in you shopping cart at the moment")
-        return redirect(reverse('concerts'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
 
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['Full Name'],
+            'email': request.POST['Email Address'],
+            'phone_number': request.POST['Phone Number'],
+            'country': request.POST['Country'],
+            'postcode': request.POST['Postal Code'],
+            'town_or_city': request.POST['Town or City'],
+            'street_address1': request.POST['Street Address 1'],
+            'street_address2': request.POST['Street Address 2'],
+            'county': request.POST['County'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in cart.items():
+                try:
+                    concert = Concert.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            concert=concert,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for quantity in item_data.items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                concert=concert,
+                                quantity=quantity,
+                            )
+                            order_line_item.save()
+                except Concert.DoesNotExist:
+                    messages.error(request, (
+                        "A concert in your cart is not in our database. Please contact us.")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
 
-    order_form = OrderForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. Please check your info again.')
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "There's nothing in you shopping cart at the moment")
+            return redirect(reverse('concerts'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         message.warming(request, 'Stripe public key is missing. Set in your environment.')
