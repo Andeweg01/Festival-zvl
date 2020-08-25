7b2de86e0f5c260a5aecf7227d18d6fa4f6b5ce8
Sascha Andeweg <sascha@designed.irish>
2020-08-25 21:28:42 +0000

    finished webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -30,7 +30,7 @@ class StripeWH_Handler:
 
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.data.charges[0].amount / 100, 2)
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         for field, value in shipping_details.address.items():
             if value == "":
