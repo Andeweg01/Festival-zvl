7cbe8c1dfe45251f671bb00101afb042b0fcf6c8
Sascha Andeweg <sascha@designed.irish>
2020-08-26 17:01:03 +0000

    profile view is working

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,15 +14,16 @@ class OrderAdmin(admin.ModelAdmin):
                        'order_total', 'grand_total',
                        'original_cart', 'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
-              'email', 'phone_number', 'country',
+    fields = ('order_number', 'user_profile', 'date',
+              'full_name', 'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'order_total', 
+              'street_address2', 'county', 'order_total',
               'grand_total', 'original_cart', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'grand_total',)
 
     ordering = ('-date',)
+
 
 admin.site.register(Order, OrderAdmin)
--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from concerts.models import Concert
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from cart.contexts import cart_contents
 
 import stripe
@@ -120,6 +122,25 @@ def checkout(request):
 def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    profile = UserProfile.objects.get(user=request.user)
+    order.user_profile = profile
+    order.save()
+
+    if save_info:
+        profile_data = {
+            'default_phone_number': order.phone_number,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_postcode': order.postcode,
+            'default_town_or_city': order.town_or_city,
+            'default_county': order.county,
+            'default_country': order.country,
+      }
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+
     messages.success(request, f'Your order {order_number} was successful \
         You will receive a confirmation on {order.email}.')
 
