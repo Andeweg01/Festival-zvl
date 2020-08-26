6385a9b3418716b8c2a822a1e0ba16217b095425
Sascha Andeweg <sascha@designed.irish>
2020-08-26 14:13:04 +0000

    added profile form, views and updated template

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -7,6 +8,12 @@ from .forms import UserProfileForm
 def profile(request):
     """ display the user profile """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            message.success(request, 'Profile updated successfully')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
