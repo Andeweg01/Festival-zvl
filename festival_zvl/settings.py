a9adb6d3a40be8afa6c2050c369699f1f3d5666a
Sascha Andeweg <sascha@designed.irish>
2020-08-31 09:26:57 +0000

    set up the collectstatic to S3 functionality

--- festival_zvl/settings.py
+++ festival_zvl/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
     # other
     'crispy_forms',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -176,6 +177,23 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if 'USE_AWS' in os.environ:
+    # AWS bucket configuration
+    AWS_STORAGE_BUCKET_NAME = 'andeweg-festival-zvl'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}'.s3.amazonaws.com
+
+    # Storage Static and Media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override the URL's for static and media files in production
+    STATIC_URL = f'{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
 STRIPE_CURRENCY = 'eur'
